f53e404b1fa7f13a7179f5e526f9b5760a8291f8
lukasz <wuzu222@gmail.com>
2021-04-11 23:09:55 +0200

    Dodaj komentarz

--- src/main/java/com/github/lukaszprokop/generyki/extwo/Printer.java
+++ src/main/java/com/github/lukaszprokop/generyki/extwo/Printer.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class ListPrinter {
-    // TODO: napraw tą klasę tak aby po wszystkim nie było ostrzeżenia o surowym użyciu listy (Raw use)
+    // TODO: Napraw tą klasę tak aby po wszystkim nie było ostrzeżenia o surowym użyciu listy (Raw use),
+    //  dlaczego takie przeładowanie nie działa?
     void print(List<String> listOne, List<String> listTwo){
         IntStream.range(0, listOne.size())
                 .forEach(v -> System.out.printf(listOne.get(v) + " = " + listTwo.get(v) + "%n"));
@@ -17,6 +18,7 @@ class ListPrinter {
 }
 
 class ArrayPrinter {
+    //TODO: Przykład działającego przeładowania metody
     void print(String[] arrayOne, String[] arrayTwo){
         IntStream.range(0, arrayOne.length)
                 .forEach(v -> System.out.printf(arrayOne[v] + " = " + arrayTwo[v] + "%n"));
