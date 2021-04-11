6d2667c364d50a5d9471707e96678c4e791f5a63
lukasz <wuzu222@gmail.com>
2021-04-11 23:50:30 +0200

    Dodaj zadanie nr1

---
+++ src/main/java/com/github/lukaszprokop/generyki/exone/CosNaStart.java
@@ -0,0 +1,23 @@
+package com.github.lukaszprokop.generyki.exone;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println(new CosNaStart(1));
+        System.out.println(new CosNaStart("1"));
+    }
+}
+
+class CosNaStart {
+// TODO: Przerób tą klasę tak aby można było uruchomić Main.main()
+    private String number;
+
+    public CosNaStart(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return number;
+    }
+}
+
